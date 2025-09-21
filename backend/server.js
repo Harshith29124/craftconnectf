ae7b343daf61edc6bcf8e4e37de77bb6357ddf93
microbhuvan <microbhuvan@gmail.com>
2025-09-21 19:17:59 +0530

    trigger deploy

--- backend/server.js
+++ backend/server.js
@@ -20,9 +20,9 @@ const aiplatform = new PredictionServiceClient({
 console.log("Server.js: Attempting to connect to MongoDB...");
 // Connect to MongoDB
 mongoose
-  .connect(process.env.MONGO_URI)
-  .then(() => console.log('Server.js: MongoDB Connected successfully'))
-  .catch((err) => console.error('Server.js: MongoDB connection error:', err));
+  .connect(process.env.MONGODB_URI)
+  .then(() => console.log("Server.js: MongoDB Connected successfully"))
+  .catch((err) => console.error("Server.js: MongoDB connection error:", err));
 
 const app = express();
 
